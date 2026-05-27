b311de282526bf31ddcaace3fd8dbeb8235473ca
maheshk1109 <mahesh.k@zemosolabs.com>
2026-05-27 16:49:44 +0530

    AWS client configuration completed

--- src/main/java/com/lastmile/App.java
+++ src/main/java/com/lastmile/App.java
@@ -1,13 +1,15 @@
 package com.lastmile;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.lastmile.config.AwsConfig;
+
+public class App {
+    public static void main(String[] args) {
+        AwsConfig config = new AwsConfig();
+
+        System.out.println("DynamoDB Client: " + config.dynamoDbClient());
+        System.out.println("SQS Client: " + config.sqsClient());
+        System.out.println("SNS Client: " + config.snsClient());
+
+        System.out.println("AWS setup verified");
     }
 }
--- src/main/java/com/lastmile/config/AwsConfig.java
+++ src/main/java/com/lastmile/config/AwsConfig.java
@@ -1,10 +1,27 @@
 package com.lastmile.config;
 
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.sns.SnsClient;
+import software.amazon.awssdk.services.sqs.SqsClient;
+
 public class AwsConfig {
 
-    public void configureClients() {
-        System.out.println("DynamoDB Client Configured");
-        System.out.println("SQS Client Configured");
-        System.out.println("SNS Client Configured");
+    public DynamoDbClient dynamoDbClient() {
+        return DynamoDbClient.builder()
+                .region(Region.AP_SOUTH_1)
+                .build();
+    }
+
+    public SqsClient sqsClient() {
+        return SqsClient.builder()
+                .region(Region.AP_SOUTH_1)
+                .build();
+    }
+
+    public SnsClient snsClient() {
+        return SnsClient.builder()
+                .region(Region.AP_SOUTH_1)
+                .build();
     }
 }
